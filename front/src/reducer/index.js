42bc9b59e304987a4848d740b948503cdc782518
safwen-code <safwendjebbi1234@gmail.com>
2022-03-11 23:33:55 +0100

    alert reducer

--- front/src/reducer/index.js
+++ front/src/reducer/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 import auth from './auth'
-
+import alert from './alert'
 const combinereducer = combineReducers({
   auth,
+  alert,
 })
 
 export default combinereducer
