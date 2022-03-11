eb6ac583a272da293a9cc74e02033f4461d9a80f
safwen-code <safwendjebbi1234@gmail.com>
2022-03-11 16:20:20 +0100

    login action finished

--- front/src/action/authAction.js
+++ front/src/action/authAction.js
@@ -1,17 +1,49 @@
-import { LOGIN_USER, REGISTER_USER, ERROR_AUTH } from './types'
+import axios from 'axios'
+import { LOGIN_USER, REGISTER_USER, ERROR_LOGIN, ERROR_REGISTER } from './types'
 
-export const LoginUser = async () => {
+//Login User
+export const LoginUser = (email, password) => async (dispatch) => {
   try {
-    console.log('LoginUser')
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const body = JSON.stringify({ email, password })
+    const res = await axios.post('/auth/login', body, config)
+    dispatch({
+      type: LOGIN_USER,
+      payload: res.data,
+    })
+    //load user
   } catch (error) {
     console.error('err login')
+    dispatch({
+      type: ERROR_LOGIN,
+    })
   }
 }
 
-export const RegisterUser = async () => {
+//register User
+export const RegisterUser = ({ name, email, password }) => async (dispatch) => {
   try {
-    console.log('RegisterUser')
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const body = JSON.stringify({ name, email, password })
+    const res = await axios.post('/user/register', body, config)
+    dispatch({
+      type: REGISTER_USER,
+      payload: res.data,
+    })
+    //load user
+    //clear profile
   } catch (error) {
-    console.error('err register')
+    dispatch({
+      type: ERROR_REGISTER,
+    })
+    //load user
   }
 }
--- front/src/action/types.js
+++ front/src/action/types.js
@@ -1,3 +1,4 @@
 export const LOGIN_USER = 'LOGIN_USER'
 export const REGISTER_USER = 'REGISTER_USER'
-export const ERROR_AUTH = 'ERROR_AUTH'
+export const ERROR_LOGIN = 'ERROR_LOGIN'
+export const ERROR_REGISTER = 'ERROR_REGISTER'
