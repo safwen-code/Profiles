b60cd2b70529bc980a310f05e1e38dca18d29d47
safwen-code <safwendjebbi1234@gmail.com>
2022-03-11 23:34:39 +0100

    alert Ui

---
+++ front/src/components/layout/Alert.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Alert = ({ alert }) => {
+  return (
+    alert != null &&
+    alert.length > 0 &&
+    alert.map((el) => (
+      <div class={`alert ${el.type}`} role="alert">
+        {el.msg}
+      </div>
+    ))
+  )
+}
+const mapStateToProps = (state) => ({
+  alert: state.alert,
+})
+export default connect(mapStateToProps)(Alert)
