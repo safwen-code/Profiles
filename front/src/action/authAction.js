11df90d9a2175ebd72f26087a8bb2e034f193aad
safwen-code <safwendjebbi1234@gmail.com>
2022-03-11 13:40:26 +0100

    login & register action

---
+++ front/src/action/authAction.js
@@ -0,0 +1,17 @@
+import { LOGIN_USER, REGISTER_USER, ERROR_AUTH } from './types'
+
+export const LoginUser = async () => {
+  try {
+    console.log('LoginUser')
+  } catch (error) {
+    console.error('err login')
+  }
+}
+
+export const RegisterUser = async () => {
+  try {
+    console.log('RegisterUser')
+  } catch (error) {
+    console.error('err register')
+  }
+}
