f7a192c09cfb925086c2a6b499e7a33e715339a6
safwen-code <safwendjebbi1234@gmail.com>
2022-03-12 00:51:46 +0100

    update header

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Logout } from '../../action/authAction'
 
-const Header = (props) => {
+const Header = ({ auth: { loading, isAuth }, Logout }) => {
+  const guestUser = (
+    <Fragment>
+      <li class="nav-item">
+        <Link class="nav-link active" aria-current="page" to="/">
+          list User
+        </Link>
+        <li class="nav-item">
+          <Link class="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/register">
+            register
+          </Link>
+        </li>
+      </li>
+    </Fragment>
+  )
+  const authUser = (
+    <Fragment>
+      <li class="nav-item">
+        <Link class="nav-link" to="/profile">
+          profile
+        </Link>
+      </li>
+      <li className="na-item">
+        <Link className="nav-link" aria-current="page" to="/" onClick={Logout}>
+          logOut
+        </Link>
+      </li>
+    </Fragment>
+  )
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <Link class="navbar-brand" to="/">
           Navbar
-        </a>
+        </Link>
         <button
           class="navbar-toggler"
           type="button"
@@ -22,26 +57,7 @@ const Header = (props) => {
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/register">
-                register
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/profile">
-                profile
-              </Link>
-            </li>
+            {!loading && <Fragment>{isAuth ? authUser : guestUser}</Fragment>}
           </ul>
         </div>
       </div>
@@ -51,4 +67,7 @@ const Header = (props) => {
 
 Header.propTypes = {}
 
-export default Header
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+})
+export default connect(mapStateToProps, { Logout })(Header)
