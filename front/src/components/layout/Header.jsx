ac8ac80069c727c0dc9bb960175ec34b40a19fee
safwen-code <safwendjebbi1234@gmail.com>
2022-03-11 01:10:14 +0100

    nabar,spinner & footer layouts

---
+++ front/src/components/layout/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+const Header = (props) => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/register">
+                register
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/profile">
+                profile
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+Header.propTypes = {}
+
+export default Header
---
+++ front/src/components/layout/Spinne.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinne = () => {
+  return (
+    <div class="spinner-grow text-secondary text-center" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  )
+}
+
+export default Spinne
