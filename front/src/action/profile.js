59d6d593b9cae1779189bbd2d2124b1006c906ae
safwen-code <safwendjebbi1234@gmail.com>
2022-03-15 00:43:45 +0100

    get current profile

--- front/src/action/profile.js
+++ front/src/action/profile.js
@@ -11,10 +11,53 @@ import axios from 'axios'
 import { setAlert } from './AlertAction'
 
 // getcurrentProfile
-export const getCurrentProfile = () => async (dispatch) => {}
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    //get the data
+    const res = await axios.get('/profile/me')
+    console.log(res)
+    //disptach to reducer
+    dispatch({
+      type: PROFILE_USER,
+      payload: res.data,
+    })
+  } catch (error) {
+    //dispatch error
+    console.error('error')
+  }
+}
 
 //create Profile
-export const createProfile = () => async (dispatch) => {}
+export const createProfile = (FormData, navigate, edite = false) => async (
+  dispatch,
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const res = await axios.post('/profile/addProfile', FormData, config)
+    console.log('create profile', res.data)
+    dispatch({
+      type: PROFILE_USER,
+      payload: res.data,
+    })
+
+    dispatch(setAlert('profile created', 'alert-success'))
+
+    navigate('/profile')
+  } catch (err) {
+    const errors = err.response.data.errors
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'alert-danger')))
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
 
 //add Experience
 export const Addexperience = () => async (dispatch) => {}
