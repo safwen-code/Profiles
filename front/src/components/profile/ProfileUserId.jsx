cdff52961470b0786359d86a4e396653784b8d6e
safwen-code <safwendjebbi1234@gmail.com>
2022-03-15 16:32:57 +0100

    profile by user id is not complet

--- front/src/components/profile/ProfileUserId.jsx
+++ front/src/components/profile/ProfileUserId.jsx
@@ -1,50 +1,66 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { getProfileByUserID } from '../../action/profile'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import Spinne from '../../components/layout/Spinne'
+import { Link } from 'react-router-dom'
 
-const ProfileUserId = ({ getProfileByUserID }) => {
+const ProfileUserId = ({
+  getProfileByUserID,
+  profile: { profile, loading },
+}) => {
   let { id } = useParams()
 
   useEffect(() => {
     getProfileByUserID(id)
   }, [getProfileByUserID, id])
 
-  return (
-    <div className="container-md">
-      <div class="row">
-        <div class="col-5 tex-center offset-md-3 mb-4 mt-3">
-          <div class="card">
-            <div class="card-body text-center">this profile for userName</div>
-          </div>
-        </div>
-        <div className="row mt-3 ms-3 border border-dark">
-          <div class="col-4">
-            <img src="..." class="card-img-top" alt="..." />
-          </div>
-          <div class="col-6">
-            <div className="card">
-              <div className="card-body">
-                <h5 class="card-title">user Email</h5>
-                <p class="card-text">status</p>
-                <p className="card-text">company</p>
-                <p></p>
+  return profile === null && loading ? (
+    <Spinne />
+  ) : (
+    <Fragment>
+      <div className="container-md">
+        <Link to="/" className="btn btn-secondary mt-3">
+          Go Back
+        </Link>
+        <div class="row">
+          <div class="col-5 tex-center offset-md-3 mb-4 mt-3">
+            <div class="card">
+              <div class="card-body text-center">
+                this profile for {profile.user.name}
               </div>
             </div>
           </div>
-        </div>
-        <div class="row gx-5 mt-4 mb-4">
-          <div class="col">
-            <div class="p-3 border bg-light">skills</div>
+          <div className="row mt-3 ms-3 border border-dark">
+            <div class="col-4">
+              <img src={profile.imageUrl} class="card-img-top" alt="..." />
+            </div>
+            <div class="col-6 align-items-center pt-5 ms-5">
+              <div className="card">
+                <div className="card-body">
+                  <h5 class="card-title">{profile.user.email}</h5>
+                  <p class="card-text">{profile.status}</p>
+                  <p className="card-text">{profile.company}</p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col">
-            <div class="p-3 border bg-light">githubusername social thing</div>
+          <div class="row gx-5 mt-4 mb-4">
+            <div class="col">
+              <div class="p-3 border bg-light">skills</div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">githubusername social thing</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
-const mapStateToProps = (state) => ({})
-export default connect(null, { getProfileByUserID })(ProfileUserId)
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+})
+export default connect(mapStateToProps, { getProfileByUserID })(ProfileUserId)
